fa49a4ce727dd875c23163ac8792660c7800705d
Dragonicorn <112576735+Dragonicorn@users.noreply.github.com>
2025-03-18 18:26:45 -0700

    Help and Exit implemented.

--- main.go
+++ main.go
@@ -15,7 +15,45 @@ func cleanInput(text string) []string {
 	return words
 }
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+type Registry map[string]cliCommand
+
+var registry Registry
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:\n")
+	for _, cmd := range registry {
+		fmt.Printf("%s: %s\n", cmd.name, cmd.description)
+	}
+	return nil
+}
+
 func main() {
+	registry = Registry{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
+
 	var text string
 	scanner := bufio.NewScanner(os.Stdin)
 	for {
@@ -27,6 +65,16 @@ func main() {
 		}
 		//fmt.Println(text)
 		words := cleanInput(text)
-		fmt.Printf("Your command was: %s\n", words[0])
+		cmd := words[0]
+		command, ok := registry[cmd]
+		if ok {
+			err := command.callback()
+			if err != nil {
+				fmt.Println("Error returned by %s function.", command.name)
+			}
+		} else {
+			fmt.Println("Unknown command.")
+		}
+
 	}
 }
