3aedd461012acea942d35cb4a069e0b1b080964b
Dragonicorn <112576735+Dragonicorn@users.noreply.github.com>
2025-03-19 18:13:37 -0700

    PokeAPI implemented.

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
+	"io"
+	"net/http"
 	"os"
 	"strings"
 )
@@ -18,20 +21,37 @@ func cleanInput(text string) []string {
 type cliCommand struct {
 	name        string
 	description string
-	callback    func() error
+	callback    func(*pdConfig) error
 }
 
 type Registry map[string]cliCommand
 
 var registry Registry
 
-func commandExit() error {
+type pdConfig struct {
+	Next string
+	Prev string
+}
+
+type pdLocationArea struct {
+	Name string `json:"name"`
+	URL  string `json:"url"`
+}
+
+type pdLocationAreas struct {
+	Count   int              `json:"count"`
+	Next    string           `json:"next"`
+	Prev    string           `json:"previous"`
+	Results []pdLocationArea `json:"results"`
+}
+
+func commandExit(cfg *pdConfig) error {
 	fmt.Println("Closing the Pokedex... Goodbye!")
 	os.Exit(0)
 	return nil
 }
 
-func commandHelp() error {
+func commandHelp(cfg *pdConfig) error {
 	fmt.Println("Welcome to the Pokedex!")
 	fmt.Println("Usage:\n")
 	for _, cmd := range registry {
@@ -40,8 +60,70 @@ func commandHelp() error {
 	return nil
 }
 
+func mapArea(cfg *pdConfig, url string) error {
+	if len(url) == 0 {
+		url = "https://pokeapi.co/api/v2/location-area/"
+	}
+	res, err := http.Get(url)
+	if err != nil {
+		//log.Fatal(err)
+		return err
+	}
+	body, err := io.ReadAll(res.Body)
+	res.Body.Close()
+	if res.StatusCode > 299 {
+		//log.Fatalf("Response failed with status code: %d and\nbody: %s\n", res.StatusCode, body)
+		return err
+	}
+	if err != nil {
+		//log.Fatal(err)
+		return err
+	}
+	data := pdLocationAreas{}
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		return err
+	}
+	// fmt.Printf("Count: %d\n", data.Count)
+	// fmt.Printf("Next: '%s'\n", data.Next)
+	cfg.Next = data.Next
+	// fmt.Printf("Prev: '%s'\n", data.Prev)
+	cfg.Prev = data.Prev
+	for i, _ := range data.Results {
+		//fmt.Printf("'%s': '%s'\n", data.Results[i].Name, data.Results[i].URL)
+		fmt.Println(data.Results[i].Name)
+	}
+	return nil
+}
+
+func commandMap(cfg *pdConfig) error {
+	//fmt.Printf("Previous: %s; Next: %s\n", cfg.Prev, cfg.Next)
+	err := mapArea(cfg, cfg.Next)
+	return err
+}
+
+func commandMapB(cfg *pdConfig) error {
+	if cfg.Prev == "" {
+		fmt.Println("you're on the first page")
+		return nil
+	}
+	//fmt.Printf("Previous: %s; Next: %s\n", cfg.Prev, cfg.Next)
+	err := mapArea(cfg, cfg.Prev)
+	return err
+}
+
 func main() {
 	registry = Registry{
+		"map": {
+			name:        "map",
+			description: "Displays a list of location areas in the Pokemon world",
+			callback:    commandMap,
+		},
+		"mapb": {
+			name:        "mapb",
+			description: "Displays previous list of location areas in the Pokemon world",
+			callback:    commandMapB,
+		},
 		"help": {
 			name:        "help",
 			description: "Displays a help message",
@@ -54,6 +136,10 @@ func main() {
 		},
 	}
 
+	pdCfg := &pdConfig{
+		Next: "",
+		Prev: "",
+	}
 	var text string
 	scanner := bufio.NewScanner(os.Stdin)
 	for {
@@ -68,9 +154,9 @@ func main() {
 		cmd := words[0]
 		command, ok := registry[cmd]
 		if ok {
-			err := command.callback()
+			err := command.callback(pdCfg)
 			if err != nil {
-				fmt.Println("Error returned by %s function.", command.name)
+				fmt.Printf("Error %v returned by %s function.\n", err, command.name)
 			}
 		} else {
 			fmt.Println("Unknown command.")
